9fa0c9cb9e5271df4604e29d73240e4b9d3e301b
Melker Narikka <meklu@meklu.org>
2020-04-07 22:52:52 +0300

    ht: Yleistä pari metodia

--- patkis/src/main/java/org/meklu/patkis/dao/DBDao.java
+++ patkis/src/main/java/org/meklu/patkis/dao/DBDao.java
@@ -1,12 +1,52 @@
 
 package org.meklu.patkis.dao;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import org.meklu.patkis.domain.Database;
+import org.meklu.patkis.domain.Triple;
 
 abstract public class DBDao<T> implements Dao<T> {
     protected Database db;
 
     public DBDao(Database db) {
         this.db = db;
+    }
+
+    public T find(String field, String value) {
+        ResultSet rs = db.find(this.tableName(), field, value);
+        try {
+            if (!rs.next()) { return null; }
+        } catch (Exception e) {
+            return null;
+        }
+        return this.fromResultSet(rs);
+    }
+
+    public List<T> findLike(String field, String pattern) {
+        List<T> ret = new ArrayList<>();
+        try {
+            ResultSet rs = db.findLike(this.tableName(), field, pattern);
+            while (rs != null && rs.next()) {
+                T obj = fromResultSet(rs);
+                if (null == obj) { continue; }
+                ret.add(obj);
+            }
+        } catch (Exception e) {}
+        return ret;
+    }
+
+    public List<T> findWhere(List<Triple<String, String, String>> fields, List<String> additionalOrders) {
+        List<T> ret = new ArrayList<>();
+        try {
+            ResultSet rs = db.findWhere(this.tableName(), fields, additionalOrders);
+            while (rs != null && rs.next()) {
+                T obj = fromResultSet(rs);
+                if (null == obj) { continue; }
+                ret.add(obj);
+            }
+        } catch (Exception e) {}
+        return ret;
     }
 }
--- patkis/src/main/java/org/meklu/patkis/dao/DBUserDao.java
+++ patkis/src/main/java/org/meklu/patkis/dao/DBUserDao.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.meklu.patkis.domain.Database;
 import org.meklu.patkis.domain.Pair;
-import org.meklu.patkis.domain.Triple;
 import org.meklu.patkis.domain.User;
 
 public class DBUserDao extends DBDao<User> implements UserDao {
@@ -32,46 +31,7 @@ public class DBUserDao extends DBDao<User> implements UserDao {
 
     @Override
     public boolean update(User u) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public User find(String field, String value) {
-        ResultSet rs = db.find(this.tableName(), field, value);
-        try {
-            if (!rs.next()) { return null; }
-        } catch (Exception e) {
-            return null;
-        }
-        return this.fromResultSet(rs);
-    }
-
-    @Override
-    public List<User> findLike(String field, String pattern) {
-        List<User> ret = new ArrayList<>();
-        try {
-            ResultSet rs = db.findLike(this.tableName(), field, pattern);
-            while (rs != null && rs.next()) {
-                User u = fromResultSet(rs);
-                if (null == u) { continue; }
-                ret.add(u);
-            }
-        } catch (Exception e) {}
-        return ret;
-    }
-
-    @Override
-    public List<User> findWhere(List<Triple<String, String, String>> fields, List<String> additionalOrders) {
-        List<User> ret = new ArrayList<>();
-        try {
-            ResultSet rs = db.findWhere(this.tableName(), fields, additionalOrders);
-            while (rs != null && rs.next()) {
-                User u = fromResultSet(rs);
-                if (null == u) { continue; }
-                ret.add(u);
-            }
-        } catch (Exception e) {}
-        return ret;
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
