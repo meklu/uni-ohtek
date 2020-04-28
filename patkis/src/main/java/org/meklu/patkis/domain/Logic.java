a5304e00adf47fe333c3c4b705be58b5fa7b5b2e
Melker Narikka <meklu@meklu.org>
2020-04-28 14:14:47 +0300

    ht: Logic: javadoc

--- patkis/src/main/java/org/meklu/patkis/domain/Logic.java
+++ patkis/src/main/java/org/meklu/patkis/domain/Logic.java
@@ -4,6 +4,8 @@ package org.meklu.patkis.domain;
 import java.util.List;
 import org.meklu.patkis.dao.*;
 
+/** Handles application logic
+ */
 public class Logic {
     private Database db;
 
@@ -13,6 +15,13 @@ public class Logic {
 
     private User currentUser;
 
+    /** Instantiates a Logic object
+     *
+     * @param db The Database to associate with this instance
+     * @param ud The UserDao to use
+     * @param sd The SnippetDao to use
+     * @param td The TagDao to use
+     */
     public Logic(Database db, UserDao ud, SnippetDao sd, TagDao td) {
         this.db = db;
         db.createTables();
@@ -23,10 +32,19 @@ public class Logic {
         this.currentUser = null;
     }
 
+    /** Gets the currently logged in user
+     *
+     * @return An instance of User or null if no one's logged in
+     */
     public User getCurrentUser() {
         return currentUser;
     }
 
+    /** Logs in a user by username
+     *
+     * @param username
+     * @return True if the user was successfully logged in, false otherwise
+     */
     public boolean login(String username) {
         User u = ud.findByLogin(username);
         if (u == null) {
@@ -36,19 +54,33 @@ public class Logic {
         return true;
     }
 
+    /** Logs out the currently logged in user
+     */
     public void logout() {
         this.currentUser = null;
     }
 
+    /** Registers a user
+     *
+     * @param user A new User to register
+     * @return True when the User object was saved successfully
+     */
     public boolean register(User user) {
         return this.ud.save(user);
     }
 
+    /** Resets the application database
+     */
     public void resetDb() {
         this.db.reset();
     }
 
+    /** Gets the Snippets visible to the current user
+     *
+     * @return The list of Snippets available
+     */
     public List<Snippet> getSnippets() {
+        //FIXME: implement properly
         return this.sd.getAll();
     }
 }
